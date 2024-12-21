012a49fb2c52f9781d8eba1314654dd29d85227f
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-12-21 15:20:37 +0530

    user signin done

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const { z } = require('zod');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const { Router } = require('express');
 
@@ -61,6 +62,66 @@ userRouter.post('/signup' , async (req , res) => {
     if (!errorFound) {
         res.json({
             message: `${username} Successfully SignedUP to VaultBox`
+        })
+    }
+})
+
+userRouter.post('/signin' , async (req , res) => {
+    // INPUT VALIDATION VIA ZOD
+    
+    const requiredBody = z.object({
+        username: z.string().min(3).max(20),
+        password: z.string().min(6)
+    })
+
+    // parse the req.body
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if (!parsedDataWithSuccess.success) {
+        res.status(400).json({
+            message: "Invaid Format Data Given",
+            errors: parsedDataWithSuccess.error.issues
+        })
+        return
+    }
+
+
+    // UPTILL here input validation is done
+
+    const { username , password } = parsedDataWithSuccess.data;
+
+    // CHECKING WHETHER THE USER EXITS IN THE DATABASE OR NOT!
+
+    const user = await UserModel.findOne({
+        username: username
+    })
+
+    if (!user) {
+        res.status(401).json({
+            message: `${username} Does Not Exists in the Database!`
+        })
+        return
+    }
+
+    // CHECK THE CREDENTIALS ARE CORRECT!
+
+    const decryptedPassoword = await bcrypt.compare(password , user.password);
+
+    if (!decryptedPassoword) {
+        res.status(403).json({
+            message: "User not Found, Incorrect Credentials!"
+        })
+    }
+    else { // JWT token Assignment
+        const token = jwt.sign({
+            id: user._id
+        } , process.env.JWT_USER_SECRET);
+
+        res.json({
+            message: `${user.firstName} ${user.lastName} Successfully LoggedIN to vaultbox!!`,
+            email: user.email,
+            token: token
         })
     }
 })
