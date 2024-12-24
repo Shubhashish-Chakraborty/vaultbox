02031438b6f26b5d76081f2811cbb151aa4fcc6e
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-12-24 13:15:50 +0530

    making backend ready to deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const { userRouter } = require("./routes/user");
 const { socialAccountRouter } = require("./routes/socialaccounts");
@@ -9,10 +10,18 @@ const { socialAccountRouter } = require("./routes/socialaccounts");
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api/v1/user' , userRouter);
 app.use('/api/v1/socialaccounts' , socialAccountRouter);
+
+app.get("/" , (req , res) => {
+    res.json({
+        message: "The Backend is UP!"
+    })
+})
 
 async function main() {
     await mongoose.connect(process.env.MONGO_URL);
